58c4eab21a7beeaf315a031e25fc447eb78814f1
Connor Hoppe <connorhoppe@gmail.com>
2021-05-10 17:04:36 -0500

    Completed all hw requirements

--- netlify/functions/movies.js
+++ netlify/functions/movies.js
@@ -7,7 +7,7 @@ let fs = require('fs')
 // defines a lambda function
 exports.handler = async function(event) {
   // write the event object to the back-end console
-  console.log(event)
+  // console.log(event)
 
   // read movies CSV file from disk
   let moviesFile = fs.readFileSync(`./movies.csv`)
@@ -25,7 +25,7 @@ exports.handler = async function(event) {
   if (year == undefined || genre == undefined) {
     return {
       statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
-      body: `Nope!` // a string of data
+      body: `Please specify year and genre!` // a string of data
     }
   }
   else {
@@ -34,14 +34,39 @@ exports.handler = async function(event) {
       movies: []
     }
 
-    for (let i=0; i < moviesFromCsv.length; i++) {
+console.log(moviesFromCsv[0].startYear)
+console.log(moviesFromCsv[0].genres)
 
+    // Loop through moviesFromCsv to update returnValue
+    for (let i=0; i < moviesFromCsv.length; i++) {
+      
+      // Check if item meets both querystring parameters
+      if (moviesFromCsv[i].startYear == year && moviesFromCsv[i].genres.includes(genre)) {
+        
+        // Check if item has a genre
+        if (moviesFromCsv[i].genre == `\\N` || moviesFromCsv[i].runtimeMinutes == `\\N`) {
+          
+          // Increase increment of numResults
+          returnValue.numResults = returnValue.numResults + 1
+
+          // Create "addValue" object and input relevant information
+          let addValue = {
+            primaryTitle: moviesFromCsv[i].primaryTitle,
+            releaseYear: moviesFromCsv[i].startYear,
+            genres: moviesFromCsv[i].genres
+          }
+
+          // Add "addValue" to returnValue object movies array
+          returnValue.movies.push(addValue)
+        }
+      }
     }
 
     // a lambda function returns a status code and a string of data
     return {
       statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
-      body: `Hello from the back-end!` // a string of data
+      // Set body to stringified version of return array
+      body: JSON.stringify(returnValue) // a string of data
     }
   }
 }
